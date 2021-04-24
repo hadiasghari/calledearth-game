6dfd0f7f9e8daaf6be533780f0825cbde12c6d38
Hadi Asghari <hadiasghari@users.noreply.github.com>
2021-04-24 14:33:38 +0200

    scaled player & changed default scene

--- Main.gd
+++ Main.gd
@@ -3,7 +3,7 @@ extends Node
 func _ready():
 	# make sure level numbers are 2 digits ("01", etc.)
 	#var level_num = str(GameState.current_level).pad_zeros(2)
-	var level = "TestH0"
-	var path = 'res://levels/Level%s.tscn' % level_num
+	var level = "TestJ1"
+	var path = 'res://levels/Level%s.tscn' % level
 	var map = load(path).instance()
 	add_child(map)
