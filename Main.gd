4e87aff80bc92294263d5951bd63a5420f3735ad
Hadi Asghari <hadiasghari@users.noreply.github.com>
2021-05-01 22:25:39 +0200

    changed word font & bg label

--- Main.gd
+++ Main.gd
@@ -1,8 +1,6 @@
 extends Node
 
 func _ready():
-	# make sure level numbers are 2 digits ("01", etc.)
-	#var level_num = str(GameState.current_level).pad_zeros(2)
 	var level = "TestJ1"
 	var path = 'res://levels/Level%s.tscn' % level
 	var map = load(path).instance()
--- words/Words.gd
+++ words/Words.gd
@@ -3,5 +3,17 @@ extends RigidBody2D
 
 func init(pos, text):
 	$Label.text = text
+	#var sz = $Label.get_size()
+	#$Rect.set_size(sz)
+	var sz = Vector2(len(text) * 16, 32)
+	var sz2 = sz/2
+	$Rect.set_size(sz)
+	$Collision.shape.extents = sz2
+	$Collision.transform[2] = sz2  #)  # ((1, 0), (-0, 1), (52, 16))
+	print(typeof($Collision.transform))  # 8
+	#$Collision.Shape.  set_size(sz)
+	# - TODO: can I make the label centered?
+	# - shall we insert line breaks after certain characters? J=> no
+	# - font-size the same for now 
 	position = pos
 
