886bd02ea6563fc55d264c14d0371fbbf5fb8032
Hadi Asghari <hadiasghari@users.noreply.github.com>
2021-05-02 19:29:03 +0200

    word sound appearance

--- items/Words.gd
+++ items/Words.gd
@@ -11,8 +11,8 @@ func init(pos, text, ecode):
 	$Collision.transform[2] = sz2  # ((1, 0), (-0, 1), (52, 16))
 	# note: shall we insert line breaks after certain characters? J=> no
 	# note: font-size the same for now 
+	# TODO... center label
 	position = pos
-	
-	# TODO 1. center label
-	# TODO 2. play sound!
-	# TODO 3. two types of words, jumpable (sticky), moveable
+	$Audio.stream.set_loop(false)
+	$Audio.play()
+	# TODO... two types of words, jumpable (sticky), moveable
