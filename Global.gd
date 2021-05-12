f0c5032e6c36cc64ea9f435271879f75e7ccdfc6
Hadi Asghari <hadiasghari@users.noreply.github.com>
2021-05-12 20:02:32 +0200

    reset server to heroku

--- Global.gd
+++ Global.gd
@@ -5,8 +5,8 @@ extends Node
 # ref: https://docs.godotengine.org/en/stable/getting_started/step_by_step/singletons_autoload.html
 
 var game_id = ""
-#var server_url = "https://calledearth.herokuapp.com"
-var server_url = "http://127.0.0.1:8000"
+var server_url = "https://calledearth.herokuapp.com"
+#var server_url = "http://127.0.0.1:8000"
 
 var last_save = ""
 
