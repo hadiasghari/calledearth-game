b0d6518a6f34a3d471cdac391c5a87ea8f6ecbe7
Hadi <hadiasghari@users.noreply.github.com>
2021-10-01 19:11:28 +0200

    added +/-1 to powerups for clarity

--- items/Energy.gd
+++ items/Energy.gd
@@ -9,11 +9,15 @@ var textures = {'l': 'res://assets/art/energy/love.png',
 func init(type, _emoji, pos):
 	$Sprite.texture = load(textures[type])	
 	position = pos
+	if type == "s" or type == "m":
+		$LabelPoints.text = "-1"
+		$LabelPoints.add_color_override("font_color", "#ff0000")
 
 func _ready():
 	$Audio.play()  # make sure not looping!
 
 func _on_Timer_timeout():	
+	$LabelPoints.visible = false
 	$Tween1.interpolate_property($Sprite, "scale",
 			Vector2(0.3, 0.3), Vector2(0.01, 0.01), 1,
 			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
